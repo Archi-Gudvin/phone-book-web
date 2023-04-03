bed42ebafe4b39170c03358ea9c73cef27de958b
Мисюк Александр <masalskyas@gmail.com>
2023-04-03 12:52:59 +0300

    changed  Client.cs and removed unnecessary classes

--- WpfApp/WpfApp/Models/Client.cs
+++ WpfApp/WpfApp/Models/Client.cs
@@ -1,17 +1,110 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace WpfApp.Models
 {
-    public class Client
+    public class Client : INotifyPropertyChanged
     {
+        string lastname;
+        string firstName;
+        string? patronymic;
+        string phoneNumber;
+        string address;
+        string? desciption;
+
+
+        /// <summary>
+        /// ID клиента
+        /// </summary>
         public int Id { get; set; }
-        public string LastName { get; set; }
-        public string FirstName { get; set; }
-        public string Patronymic { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Address { get; set; }
-        public string Desciption { get; set; }
+
+        /// <summary>
+        /// Фамилия клиента
+        /// </summary>
+        public string LastName
+        {
+            get { return lastname; }
+            set
+            {
+                lastname = value;
+                OnPropertyChanged("LastName");
+            }
+        }
+
+        /// <summary>
+        /// Имя клиента
+        /// </summary>
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                firstName = value;
+                OnPropertyChanged("FirstName");
+            }
+        }
+
+
+        /// <summary>
+        /// Отчество клиента
+        /// </summary>
+        public string? Patronymic
+        {
+            get { return patronymic; }
+            set
+            {
+                patronymic = value;
+                OnPropertyChanged("Patronymic");
+            }
+        }
+
+        /// <summary>
+        /// Номер телефона клиента
+        /// </summary>
+        public string PhoneNumber
+        {
+            get { return phoneNumber; }
+            set
+            {
+                phoneNumber = value;
+                OnPropertyChanged("PhoneNumber");
+            }
+        }
+
+        /// <summary>
+        /// Адрес клиента
+        /// </summary>
+        public string Address
+        {
+            get { return address; }
+            set
+            {
+                address = value;
+                OnPropertyChanged("Address");
+            }
+        }
+
+        /// <summary>
+        /// Описание клиента
+        /// </summary>
+        public string? Desciption
+        {
+            get { return desciption; }
+            set
+            {
+                desciption = value;
+                OnPropertyChanged("Desciption");
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
     }
 }
---
+++ WpfApp/WpfApp/ViewModel/ClientViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace WpfApp.ViewModel
+{
+    class ClientViewModel : INotifyPropertyChanged
+    {
+    }
+}
